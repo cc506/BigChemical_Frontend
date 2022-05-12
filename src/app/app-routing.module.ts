ac094976b71f38c3ace79bc6af9b2824ae1275da
cc506 <cchung28@ucmerced.edu>
2022-05-12 14:12:02 -0700

    routing for toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessRightsComponent } from './access-rights/access-rights.component';
+import { AccessComponent } from './access/access.component';
 import { DrugComponent } from './drug/drug.component';
 import { GenerateIdComponent } from './generate-id/generate-id.component';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RepairsComponent } from './repairs/repairs.component';
+import { SensorsComponent } from './sensors/sensors.component';
 
 const routes: Routes = [
   { path: 'employee', component: ProductListComponent},
   { path: 'drug', component: DrugComponent },
   { path: 'generate-id' , component: GenerateIdComponent },
   { path: 'home' , component: HomeComponent},
+  { path: 'sensors', component: SensorsComponent},
+  { path: 'repairs', component: RepairsComponent},
+  { path: 'access', component: AccessComponent},
+  { path: 'access-rights', component: AccessRightsComponent}
 
 ];
 
